ca08a8bf191b7d10721ad947b38d0a567fd4771a
Louis Godfrey <lginn26@users.noreply.github.com>
2021-10-16 13:28:56 -0400

    imported pygame, added window projecting logic

--- guiDemo.py
+++ guiDemo.py
@@ -1,18 +1,20 @@
+import pygame
+pygame.init()
 
 # Program Variables
 runProgram = True # will keep the program running as long 
 
 ## Game States
-TITLE = 0
+SPLASH = 0
 DIFF_SELECT = 1
 HELP = 2
 IN_GAME = 3
 
-gameState = TITLE
+gameState = SPLASH
 
 ## Window Variables
 WIDTH = 1000
-HEIGHT = 1000
+HEIGHT = 700
 SIZE = (WIDTH, HEIGHT)
 TITLE = "Untitled Typing Game"
 screen = pygame.display.set_mode(SIZE)
@@ -25,7 +27,9 @@ display_clock = 0
 # Asset Variables
 
 ## Fonts
-titleFont = 
+
+## Colors
+COL_BLACK = (0, 0, 0)
 
 # Helper Functions
 def updateScreen():
@@ -37,14 +41,15 @@ def updateScreen():
 while runProgram:
     # Handling Logic for game state events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
-            gameState = False
+        if event.type == pygame.QUIT:
+            runProgram = False
     
     # Logic for game states
-    if gameState == TITLE: # Logic for the title screen
+    if gameState == SPLASH: # Logic for the title screen
         # Perfoming Logic
         pass
         # Drawing To Screen 
+        pygame.Surface.fill(screen, COL_BLACK)
 
     elif gameState == DIFF_SELECT: # Logic for the difficulty select screen
         pass
